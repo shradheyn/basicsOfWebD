dde3073f68a4f02cf3ca2e6e6124e9671ddca3e8
Shradhey <115147605+shradheyn@users.noreply.github.com>
2025-09-17 15:14:44 +0530

    Add files via upload

---
+++ javascript/hello.js
@@ -0,0 +1,9 @@
+document.writeln("<h3> JS Code Created by Shradhey Naudiyal</h3>")
+
+var num1=100,num2=300,num3=500;
+
+sum=num1+num2+num3;
+
+document.writeln("The Sum of 3 Numbers is : "+sum+"<br>");
+avg=sum/3;
+document.writeln("The Average of 3 Numbers is : "+avg);
---
+++ javascript/sumofseries.js
@@ -0,0 +1,7 @@
+var n,i=1,sum=0;
+        n=parseInt(prompt("Enter a positive integer :"));
+        for(i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        document.write("<h2>The sum of series is : "+ sum+"</h2>");
+        alert("The sum of series is : "+ sum);
